10a0342d2a5ba8b4c588f97e633c6ce020c0d111
alimsadetov <alimsadetov2019@mail.ru>
2022-04-03 01:03:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="body">
+  <Categories />
+  </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue';
+import Categories from './components/Categories.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Categories
+  }
+});
+</script>
+
+<style lang="scss">
+.body{
+  background-color: #000000;
+  background-image: linear-gradient(315deg, #000000 0%, #414141 74%);
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/Categories.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="title">Категории</div>
+<div class="container" >
+  <img class="category" v-for="i in categories" :key="i" :src="require(`@/${i.image}`)" />
+</div>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Categories',
+  data(){
+    return {
+      categories: [
+        {
+          name: "Казуальные",
+          image: "assets/casualcategory.jpg"
+        },
+        {
+          name: "Аркада",
+          image: "../assets/arcadescategory.jpg"
+        },
+        {
+          name: "Азартные",
+          image: "../assets/azartcategory.jpg"
+        },
+        {
+          name: "Приключения",
+          image: "../assets/adventurecategory.jpg"
+        },
+        {
+          name: "Симуляторы",
+          image: "../assets/simulatorcategory.jpeg"
+        },
+        {
+          name: "Стратегии",
+          image: "../assets/strategiescategory.jpg"
+        },
+      ]
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.container{
+  max-width: 1100px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-row-gap: 40px;
+}
+
+.category{
+  height: 185px;
+  width: 185px;
+  background-color: white;
+  align-text: center;
+  box-shadow: 0 20px 50px rgba(0,0,0,.8);
+  justify-self: center;
+}
+
+.category:hover{
+  box-shadow: 0 0px 10px rgba(255,255,255);
+}
+
+.title{
+  padding-top: 50px;
+  margin: 0 auto;
+  font-size: 30px;
+  color: white;
+}
+
+
+  
+</style>
